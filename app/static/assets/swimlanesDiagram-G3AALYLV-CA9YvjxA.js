0beee9482e6ce797595b9b81727f347de5565ba6
dharaneesh-r <investoproweal@gmail.com>
2026-07-05 17:39:06 +0530

    updates on the chunks and that is deleted

--- app/static/assets/swimlanesDiagram-G3AALYLV-CA9YvjxA.js
+++
@@ -1,8 +0,0 @@
-import{c as e,s as r}from"./flowDiagram-23GEKE2U-CK6hIE82.js";import{_ as a}from"./index-D0yWciYv.js";import"./chunk-5VM5RSS4-OuI641XT.js";import"./chunk-XXDRQBXY-9zK1fXW_.js";import"./chunk-VR4S4FIN-tPbCNG8e.js";import"./chunk-32BRIVSS-BoSU4daH.js";import"./channel-DKwdze0t.js";var o=a(t=>`${r(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
